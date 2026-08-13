05b30d81e254fc21673b29e66207c8acd5d70066
MuhammadAinurRofik <muhammadainur40@gmail.com>
2026-08-13 09:10:43 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,23 @@ use App\Http\Controllers\CartController;
 Route::get('/', function () {
     return view('home.index');
 });
+
 Route::get('/produk', [ProductController::class, 'index'])->name('products.index');
+
+// Routes Cart (Dengan Login)
 Route::middleware('auth')->group(function () {
     Route::get('cart', [CartController::class, 'show'])->name('cart.show');
     Route::post('cart/{product}', [CartController::class, 'add'])->name('cart.add');
     Route::delete('cart/items/{cartItem}', [CartController::class, 'remove'])->name('cart.remove');
-    Route::put('cart/items/{cartItem}', [CartController::class, 'update'])->name('cart.update');
+    Route::put('cart/items/{cartItem}', [CartController::class, 'update'])->name('cart.update'); // <-- Nama ini dibiarkan
 });
 
 // Routes Cart (Tanpa Login)
-Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
-Route::delete('/cart/{id}', [CartController::class, 'destroy'])->name('cart.destroy');
+// PERBAIKAN: Menambahkan prefix 'guest.' pada penamaan route agar tidak bentrok
+Route::get('/guest-cart', [CartController::class, 'index'])->name('guest.cart.index');
+Route::post('/guest-cart', [CartController::class, 'store'])->name('guest.cart.store');
+Route::put('/guest-cart/{id}', [CartController::class, 'update'])->name('guest.cart.update'); // <-- PERBAIKAN NAMA DI SINI
+Route::delete('/guest-cart/{id}', [CartController::class, 'destroy'])->name('guest.cart.destroy');
 
 Route::get('/cara-pemesanan', function () {
     return view('home.order');
